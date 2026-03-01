1fa45ead03767372fda44e09322e738d04bc4d53
GakuruAlex <gakurualexgicimo@gmail.com>
2026-03-01 12:13:13 +0300

    Add files

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email_address   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  role            :integer          default(0), not null
+#
+# Indexes
+#
+#  index_users_on_email_address  (email_address) UNIQUE
+#
+
+class User < ApplicationRecord
+  enum :role, { guest: 0, member: 1, admin: 2 }
+  has_secure_password
+  has_many :sessions, dependent: :destroy
+
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
+end
