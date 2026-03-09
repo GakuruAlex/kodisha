b51f60c51b047e3cfe684f03a24d51dc80a780cb
GakuruAlex <gakurualexgicimo@gmail.com>
2026-03-09 08:06:42 +0300

    Add assest to git

---
+++ db/migrate/20260308152706_add_details_to_user.rb
@@ -0,0 +1,7 @@
+class AddDetailsToUser < ActiveRecord::Migration[8.1]
+  def change
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    add_column :users, :phonenumber, :string
+  end
+end
