c48c229fb5a33fea83efe25b2b500025960e5c9e
GakuruAlex <gakurualexgicimo@gmail.com>
2026-03-25 23:21:58 +0300

    Rescue from -Add rescue from action controller parameters missing in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
+rescue_from ActionController::ParameterMissing, with: :required_params_missing
   include Authentication
   include Authorization
+
+  private
+  def required_params_missing(exception)
+    render json: { error: exception.message }, status: :bad_request
+  end
 end
