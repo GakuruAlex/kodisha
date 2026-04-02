14acbe3a2e5cb44094cce8ec6edfc6e17be0dbbc
Gakuru Alex <84031748+GakuruAlex@users.noreply.github.com>
2026-04-02 18:58:56 +0300

    Revert "User serializer"
    This reverts commit 1c0c5311656c399b50ffcfcbae83a5f2626dab00.

--- app/serializers/kodisha/user_serializer.rb
+++ app/serializers/kodisha/user_serializer.rb
@@ -1,19 +1,9 @@
 class Kodisha::UserSerializer < ActiveModel::Serializer
-  attributes :id, :phonenumber, :firstname, :lastname, :email_address, :tenant, :landlord
+  attributes :id, :phonenumber, :firstname, :lastname, :email_address
   has_one :tenant_profile
   has_one :landlord_profile
 
   def fullname
     "#{object.firstname} #{object.lastname}"
-  end
-  def tenant
-    if object.tenant_profile
-      true
-    end
-  end
-  def landlord
-   if  object.landlord_profile
-     true
-   end
   end
 end
