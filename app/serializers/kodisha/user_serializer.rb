eafb5d8d2a65e98ceb5f79944b1e76de95262374
GakuruAlex <gakurualexgicimo@gmail.com>
2026-04-02 20:28:56 +0300

    check if user is a tenant , if user ia tenant

--- app/serializers/kodisha/user_serializer.rb
+++ app/serializers/kodisha/user_serializer.rb
@@ -1,9 +1,13 @@
 class Kodisha::UserSerializer < ActiveModel::Serializer
-  attributes :id, :phonenumber, :firstname, :lastname, :email_address
-  has_one :tenant_profile
-  has_one :landlord_profile
+  attributes :id, :phonenumber, :firstname, :lastname, :email_address, :landlord, :tenant
 
   def fullname
     "#{object.firstname} #{object.lastname}"
+  end
+  def landlord
+    object.landlord_profile.present?
+  end
+  def tenant
+    object.tenant_profile.present?
   end
 end
