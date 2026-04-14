5a9975c703456394991004d8fbb09c329f254a10
GakuruAlex <gakurualexgicimo@gmail.com>
2026-04-14 20:31:37 +0300

    Add house count

--- app/serializers/landlord/estate_serializer.rb
+++ app/serializers/landlord/estate_serializer.rb
@@ -1,7 +1,4 @@
 class Landlord::EstateSerializer < ActiveModel::Serializer
-  attributes :id, :has_vacancy, :name,  :location, :num_houses
-  # has_many :houses, serializer: Landlord::Estate::HouseSerializer
-  def num_houses
-    object.houses.count
-  end
+  attributes :id, :has_vacancy, :name,  :location, :houses_count
+  has_many :houses, serializer: Landlord::Estate::HouseSerializer
 end
